1a44f86bbfd8abfa9bc1496057e80431774c2110
Peter Dietz <dietz.72@osu.edu>
2013-03-21 22:36:19 -0400

    Add relationship that Team has many Developers, and Dev belongs to Team

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,3 +1,4 @@
 class Developer < ActiveRecord::Base
+  belongs_to :team
   attr_accessible :bio, :github_username, :name, :team_id, :twitter_username, :username
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,6 @@
 class Team < ActiveRecord::Base
   belongs_to :department
-
+  has_many :developers, :dependent => :destroy
 
 
   attr_accessible :department_id, :description, :name
