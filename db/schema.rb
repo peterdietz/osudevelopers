6421a3a0d0d028233e82778dec651ecaad42395f
Peter Dietz <dietz.72@osu.edu>
2013-03-21 23:34:14 -0400

    Run migration to add applications

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130322022145) do
+ActiveRecord::Schema.define(:version => 20130322031848) do
+
+  create_table "applications", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "team_id"
+    t.string   "status"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "departments", :force => true do |t|
     t.string   "name"
