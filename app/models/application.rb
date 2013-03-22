26f09db3e2e364606d31b5131c65c302003fba51
Peter Dietz <dietz.72@osu.edu>
2013-03-21 23:34:38 -0400

    Add relationship Team has many Applications, App belongs to Team

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,5 @@
 class Application < ActiveRecord::Base
+  belongs_to :team
+
   attr_accessible :description, :name, :status, :team_id
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 class Team < ActiveRecord::Base
   belongs_to :department
   has_many :developers, :dependent => :destroy
-
+  has_many :applications, :dependent => :destroy
 
   attr_accessible :department_id, :description, :name
 end
