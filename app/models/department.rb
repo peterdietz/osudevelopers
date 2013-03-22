c5aafe75d32c54f87df6afc1db661155640e8e03
Peter Dietz <dietz.72@osu.edu>
2013-03-21 23:01:25 -0400

    Add number of developers within department

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,4 +1,13 @@
 class Department < ActiveRecord::Base
   has_many :teams, :dependent => :destroy
   attr_accessible :description, :name
+
+  def developers_within_department
+    devs = 0
+    teams.find_each do |team|
+      devs += team.developers.count
+    end
+
+    return devs
+  end
 end
